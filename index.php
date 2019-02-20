6da468aa50dbeca3d43edb20aac85706a301442b
Ilya Semenov <mr.ilya2015@mail.ru>
2019-02-20 10:06:00 +0300

    second commit

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
   
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,500&amp;subset=cyrillic" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-  <title>Юкон - сеть компаний</title>
+  <title>Юкон - сеть компаний(Тестовое сохранение)</title>
   
  </head>
  <body>
